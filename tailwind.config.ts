a13a69cb902ef48adb91d67b9f22d0f7bfd62e9d
See2021 <s6330613014@phuket.psu.ac.th>
2023-12-25 16:54:24 +0700

    fuck

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,35 +15,6 @@ const config: Config = {
       },
     },
   },
-  daisyui: {
-    
-    themes: [
-      {
-        mytheme: {
-
-          "primary": "#eab308",
-
-          "secondary": "#b91c1c",
-
-          "accent": "#3b82f6",
-
-          "neutral": "#18182f",
-
-          "base-100": "#ffffff",
-
-          "info": "#3abff8",
-
-          "success": "#22c55e",
-
-          "warning": "#fbbd23",
-
-          "error": "#e11d48",
-        },
-      },
-    ],
-  },
-  plugins: [
-    require("daisyui")
-  ],
+  plugins: [],
 }
 export default config
